33ded3b2de6870c136278d017149d0089e5ef339
Jeremy <jeremy@jeremy-ibm.local>
2024-05-03 16:12:11 +0100

    Added registry patching

--- mongo/deploy.sh
+++ mongo/deploy.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# Create a PVC for the image registry
+cat << EOF | oc create -f -
+kind: PersistentVolumeClaim
+apiVersion: v1
+metadata:
+  name: image-registry-storage 
+  namespace: openshift-image-registry 
+spec:
+  accessModes:
+  - ReadWriteOnce 
+  resources:
+    requests:
+      storage: 100Gi
+EOF
+
+# Make the image registry managed
+oc patch configs.imageregistry.operator.openshift.io cluster --type merge --patch '{"spec":{"managementState":"Managed"}}'
+# Allow block storage for the registry
+oc patch config.imageregistry.operator.openshift.io/cluster --type=merge -p '{"spec":{"rolloutStrategy":"Recreate","replicas":1}}'
+# Link the claim above to the image resgitry
+oc patch config.imageregistry.operator.openshift.io/cluster --type=merge -p '{"spec":{"storage":{"pvc":{"claim":"image-registry-storage"}}}}'
+# Exposing OCP default registry route
+oc patch configs.imageregistry.operator.openshift.io/cluster --patch '{"spec":{"defaultRoute":true}}' --type=merge
+
 HOST=$(oc get route default-route -n openshift-image-registry --template='{{ .spec.host }}')
 if ! podman login -u kubeadmin -p "$(oc whoami -t)" --tls-verify=false $HOST; then
     echo "Failed to login to the registry. Exiting."
